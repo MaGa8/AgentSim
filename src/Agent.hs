4e2097029e9a838d337615e2a0708b05d158e61a
Martin <martin@localhost.localdomain>
2021-11-14 09:25:06 +0100

    Inlude UID in Agent.  Changed Speech function. Include position.

--- src/Agent.hs
+++ src/Agent.hs
@@ -1,34 +1,39 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE Rank2Types #-}
 
 module Agent
   (
-    Agent(..), Speech, Behavior, Sight
+    Agent(..), Neighbor, Speech, Behavior, Sight
   , agentTalk, agentAct, agentSee
-  , updateCore
+  , updateCore, updatePos
   ) where
 
 import Data.Map as M
 
-type Speech p m a = p -> a -> Map a p -> Map a m
+type Neighbor d a p = (d,a,p)
+type Speech d p m a = forall t. p -> a -> t (Neighbor d a p) -> [(d,m)]
 type Behavior p m a = p -> a -> [m] -> (a,p)
 type Sight p a = p -> a -> (p,p)
 
-data Agent p m a = Agent{ talk :: Speech p m a, act :: Behavior p m a, see :: Sight p a, core :: a }
+data Agent d p m a = Agent{ talk :: Speech d p m a, act :: Behavior p m a, see :: Sight p a, core :: a, getIdent :: d, getPos :: p }
 
-instance (Eq a) => Eq (Agent p m a) where
+instance (Eq a) => Eq (Agent d p m a) where
   a1 == a2 = core a1 == core a2
 
-instance (Ord a) => Ord (Agent p m a) where
+instance (Ord a) => Ord (Agent d p m a) where
   compare a1 a2 = compare (core a1) (core a2)
 
-agentTalk :: Agent p m a -> p -> Map a p -> Map a m
-agentTalk ag pos = talk ag pos (core ag)
+agentTalk :: (Foldable t) => Agent d p m a -> t (d,a,p) -> [(d,m)]
+agentTalk ag = talk ag (getPos ag) (core ag)
 
-agentAct :: Agent p m a -> p -> [m] -> (a,p)
-agentAct ag pos = act ag pos (core ag)
+agentAct :: Agent d p m a -> [m] -> (a,p)
+agentAct ag = act ag (getPos ag) (core ag)
 
-agentSee :: Agent p m a -> p -> (p,p)
-agentSee ag pos = see ag pos (core ag)
+agentSee :: Agent d p m a -> (p,p)
+agentSee ag = see ag (getPos ag) (core ag)
 
-updateCore :: Agent p m a -> a -> Agent p m a
+updateCore :: Agent d p m a -> a -> Agent d p m a
 updateCore ag c = ag{ core = c }
+
+updatePos :: Agent d p m a -> p -> Agent d p m a
+updatePos ag pos = ag{ getPos = pos }
