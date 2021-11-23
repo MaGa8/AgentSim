98747b4c3fc9bcc82a371fcba88971e2ff77d910
Martin <martin@localhost.localdomain>
2021-11-23 08:29:00 +0100

    Replaced list by array in median of medians.

--- src/Rank.hs
+++ src/Rank.hs
@@ -11,9 +11,13 @@ import Data.List as L
 import Data.Maybe
 import Data.Ratio
 import Data.Foldable as F
+import qualified Data.Array.IArray as A
 
 type Size = Int
 type Rank = Int -- ^ rank is the number of elements smaller
+
+cons' :: a -> [a] -> [a]
+cons' x xs = x `seq` x : xs
 
 -- lazyness only pays off if we're not scanning over the two biggest lists afterwards!
 partitionByPivot :: (a -> a -> Ordering) -> a -> [a] -> ([a],[a],[a])
@@ -30,15 +34,17 @@ emptyCompo :: Compo a
 emptyCompo = ([], 0)
 
 addToCompo :: Compo a -> a -> Compo a
-addToCompo (xs, n) x = (x : xs, n+1)
+addToCompo (xs, n) x = (cons' x xs, n+1)
 
-partitionByPivot' :: Foldable t => (a -> a -> Ordering) -> a -> t a -> (Compo a, Compo a, Compo a)
-partitionByPivot' fcmp piv = foldl' categorize (emptyCompo, emptyCompo, emptyCompo)
+partitionByPivot' :: Foldable t => (a -> a -> Ordering) -> a -> t a -> (A.Array Int a, A.Array Int a, A.Array Int a)
+partitionByPivot' fcmp piv = mkArrays . foldl' categorize (emptyCompo, emptyCompo, emptyCompo)
   where
     categorize (smaller, equal, greater) x = case fcmp piv x of
       GT -> (addToCompo smaller x, equal, greater)
       EQ -> (smaller, addToCompo equal x, greater)
       LT -> (smaller, equal, addToCompo greater x)
+    mkArrays (smalls, equals, greats) = let constr (xs, size) = A.listArray (0, size - 1) xs
+                                        in (constr smalls, constr equals, constr greats)
 
 groupsOfN :: Int -> [a] -> [[a]]
 groupsOfN n xs = g : if null xs' then [] else groupsOfN n xs'
@@ -58,7 +64,7 @@ medianRank = floor . (% 2) . subtract 1
 
 -- | selects the lower median from the list
 findColMedianWorker :: (a -> a -> Ordering) -> Size -> Size -> [a] -> Maybe a
-findColMedianWorker fcmp nelem chunkSize = medianOfMediansWorker fcmp nelem' (medianRank nelem') chunkSize . map pickColMid . groupsOfN chunkSize 
+findColMedianWorker fcmp nelem chunkSize = medianOfMediansWorker fcmp nelem' (medianRank nelem') chunkSize . map pickColMid . groupsOfN chunkSize
   where
     -- Really slow for what it does.  Ensure groups themselves are evaluated strictly
     pickColMid = fromJust . pickMiddle . L.sortBy fcmp
@@ -71,16 +77,21 @@ findColMedianWorker' fcmp nelem chunkSize = medianOfMediansWorker fcmp nelem' (m
   where
     nelem' = ceiling $ nelem % chunkSize
 
-produceColMedians :: Foldable t => (a -> a -> Ordering) -> Size -> t a -> [a]
-produceColMedians fcmp nchunk = trd . finishup . foldl' (\tup x -> processGroup $ addToGroup x tup) ([], 0, [])
+produceColMedians :: Foldable t => (a -> a -> Ordering) -> Size -> t a -> A.Array Int a
+produceColMedians fcmp nchunk = mkArray . finishup . foldl' (\tup x -> processGroup $ addToGroup x tup) ([], 0, [], 0)
   where
-    processGroup (buffer, size, medians)
-      | size == nchunk = ([], 0, moveBufferMedian buffer medians)
-      | otherwise = (buffer, size, medians)
-    moveBufferMedian buffer medians = maybe medians (: medians) . pickMiddle $ L.sortBy fcmp buffer
-    addToGroup x (buffer, size, medians) = (x : buffer, size + 1, medians)
-    finishup (buffer, size, medians) = ([], 0, moveBufferMedian buffer medians)
-    trd (_,_,x) = x
+    processGroup (buffer, size, medians, nmeds)
+      | size == nchunk = ([], 0, moveBufferMedian buffer medians, nmeds + 1)
+      | otherwise = (buffer, size, medians, nmeds)
+    moveBufferMedian buffer medians = maybe medians (`cons'` medians) . pickMiddle $ L.sortBy fcmp buffer
+    addToGroup x (buffer, size, medians, nmeds) = (x : buffer, size + 1, medians, nmeds)
+    finishup a@(buffer, size, medians, nmeds)
+      | null buffer = a
+      | otherwise = ([], 0, moveBufferMedian buffer medians, nmeds + 1)
+    mkArray (_, _, meds, n) = A.listArray (0, n-1) meds
+
+arraySize :: (Num i, A.IArray a e, A.Ix i) => a i e -> i
+arraySize = (+ 1) . uncurry subtract . A.bounds
 
 -- | select element at index rank as in the median-of-medians algorithm
 -- adapted to also work with duplicate elements
@@ -95,18 +106,14 @@ medianOfMediansWorker fcmp nelem rank chunkSize xs
   | pivotLowerRank <= rank && rank <= pivotUpperRank = Just pivot            -- O(1)
   | rank < pivotLowerRank = medianOfMediansWorker fcmp nsmalls rank chunkSize smalls            -- O(smalls)
   | rank > pivotUpperRank = medianOfMediansWorker fcmp ngreats (rank - nelem + ngreats) chunkSize greats            -- O(greats) where w.c. n = smalls+greats
-  where 
+  where
     -- require 3 passes over xs (last implicit over smalls, greats)
     pivot = fromJust $ findColMedianWorker' fcmp nelem chunkSize xs -- O(?)
-    ((smalls, nsmalls), (equals, nequals), (greats, ngreats)) = partitionByPivot' fcmp pivot xs -- O(n)
+    (smalls, equals, greats) = partitionByPivot' fcmp pivot xs -- O(n)
+    (nsmalls, ngreats) = (arraySize smalls, arraySize greats)
     -- (nsmalls, ngreats) = (length smalls, length greats) -- O(n)
     (pivotLowerRank, pivotUpperRank) = (nsmalls, nelem - ngreats - 1) -- O(1)
 
--- | obtain element at index in a foldable
-atIndex :: (Foldable t) => Int -> t a -> Maybe a
-atIndex n = either Just (const Nothing) . foldl' searcher (Right n)
-  where
-    searcher box x = box >>= (\n -> if n == 0 then Left x else Right (n-1))
 
 pickMedian :: (a -> a -> Ordering) -> [a] -> Maybe a
 pickMedian fcmp xs = medianOfMediansWorker fcmp (length xs) (medianRank $ length xs) 5 xs
@@ -118,5 +125,6 @@ pickAtRank fcmp rank xs = medianOfMediansWorker fcmp (length xs) rank 5 xs
 partitionByMedian :: (a -> a -> Ordering) -> [a] -> Maybe (a,[a],[a])
 partitionByMedian fcmp xs = makePartition <$> pickMedian fcmp xs
   where
-    makePartition med = let ((smalls, nsmalls), (equals, _), (greats, ngreats)) = partitionByPivot' fcmp med xs
-                        in if nsmalls <= ngreats then (med, equals ++ smalls, greats) else (med, smalls, equals ++ greats)
+    makePartition med = let (smalls, equals, greats) = partitionByPivot' fcmp med xs
+                            (nsmalls, ngreats) = (arraySize smalls, arraySize greats)
+                        in if nsmalls <= ngreats then (med, A.elems equals ++ A.elems smalls, A.elems greats) else (med, A.elems smalls, A.elems equals ++ A.elems greats)
