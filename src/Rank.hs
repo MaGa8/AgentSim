4faad066700de092ada2aa8fe79da8f2bc23ff32
Martin <martin@localhost.localdomain>
2021-11-24 09:09:02 +0100

    Added approximate median top-level and cleaned up Rank module.

--- src/Rank.hs
+++ src/Rank.hs
@@ -4,6 +4,7 @@ module Rank
   (
     medianRank
   , pickMedian, pickAtRank
+  , pickApproxMedian
   , partitionByPivot, partitionByMedian
   , medianOfMedians, sicilianMedian
   ) where
@@ -17,35 +18,74 @@ import qualified Data.Array.IArray as A
 type Size = Int
 type Rank = Int -- ^ rank is the number of elements smaller
 
-cons' :: a -> [a] -> [a]
-cons' x xs = x `seq` x : xs
+-- | Choose median exactly.
+pickMedian :: Foldable t => (a -> a -> Ordering) -> t a -> Maybe a
+pickMedian fcmp xs = medianOfMedians fcmp (length xs) (medianRank $ length xs) 5 xs
 
--- lazyness only pays off if we're not scanning over the two biggest lists afterwards!
-partitionByPivot :: (a -> a -> Ordering) -> a -> [a] -> ([a],[a],[a])
-partitionByPivot fcmp piv = foldl categorize ([],[],[])
+-- | Chooses element whose rank is approximately the median.  No approximation guarantee.
+pickApproxMedian :: forall t a. Foldable t =>
+                    (a -> a -> Ordering) -- ^ comparator function computing Ordering of first w.r.t. latter
+                 -> t a -> Maybe a
+pickApproxMedian fcmp = uncurry (sicilianMedian fcmp 5) . boundsPair . fold2Array
   where
-    categorize (smaller, equal, greater) x = case fcmp piv x of
-      GT -> (x : smaller, equal, greater)
-      EQ -> (smaller, x : equal, greater)
-      LT -> (smaller, equal, x : greater)
+    boundsPair :: A.Array Int a -> (Int, A.Array Int a)
+    boundsPair arr = (1 + uncurry subtract (A.bounds arr), arr)
 
-type Compo a = ([a], Int)
+pickAtRank :: (a -> a -> Ordering) -> Rank -> [a] -> Maybe a
+pickAtRank fcmp rank xs = medianOfMedians fcmp (length xs) rank 5 xs
 
-emptyCompo :: Compo a
-emptyCompo = ([], 0)
+-- partitions list into values smaller or equal and values greater than the lower median 
+partitionByMedian :: (a -> a -> Ordering) -> [a] -> Maybe (a,[a],[a])
+partitionByMedian fcmp xs = makePartition <$> pickMedian fcmp xs
+  where
+    makePartition med = let (smalls, equals, greats) = mkArrayPartition $ partitionByPivot fcmp med xs
+                            (nsmalls, ngreats) = (arraySize smalls, arraySize greats)
+                        in if nsmalls <= ngreats then (med, A.elems equals ++ A.elems smalls, A.elems greats) else (med, A.elems smalls, A.elems equals ++ A.elems greats)
 
-addToCompo :: Compo a -> a -> Compo a
-addToCompo (xs, n) x = (cons' x xs, n+1)
+medianRank :: Int -> Int
+medianRank = floor . (% 2) . subtract 1
 
-partitionByPivot' :: Foldable t => (a -> a -> Ordering) -> a -> t a -> (A.Array Int a, A.Array Int a, A.Array Int a)
-partitionByPivot' fcmp piv = mkArrays . foldl' categorize (emptyCompo, emptyCompo, emptyCompo)
+partitionByPivot :: Foldable t => (a -> a -> Ordering) -> a -> t a -> (Compo a, Compo a, Compo a)
+partitionByPivot fcmp piv = foldl' categorize (emptyCompo, emptyCompo, emptyCompo)
   where
     categorize (smaller, equal, greater) x = case fcmp piv x of
       GT -> (addToCompo smaller x, equal, greater)
       EQ -> (smaller, addToCompo equal x, greater)
       LT -> (smaller, equal, addToCompo greater x)
-    mkArrays (smalls, equals, greats) = let constr (xs, size) = A.listArray (0, size - 1) xs
-                                        in (constr smalls, constr equals, constr greats)
+
+mkArrayPartition :: (Compo a, Compo a, Compo a) -> (A.Array Int a, A.Array Int a, A.Array Int a)
+mkArrayPartition (c1, c2, c3) = (compo2Array c1, compo2Array c2, compo2Array c3)
+
+-- | select element at index rank as in the median-of-medians algorithm
+-- adapted to also work with duplicate elements
+medianOfMedians :: Foldable t => (a -> a -> Ordering) -- ^ comparator: computes Ordering of the first with respect to the latter
+                -> Int -- ^ number of elements in list
+                -> Int -- ^ rank of the item to select
+                -> Int -- ^ number of elements in a column
+                -> t a -> Maybe a
+medianOfMedians fcmp nelem rank chunkSize xs
+  | null xs = Nothing
+  | nelem <= chunkSize = Just . (!! rank) . L.sortBy fcmp $ F.toList xs            -- O(1)
+  | pivotLowerRank <= rank && rank <= pivotUpperRank = Just pivot            -- O(1)
+  | rank < pivotLowerRank = medianOfMedians fcmp nsmalls rank chunkSize smalls            -- O(smalls)
+  | rank > pivotUpperRank = medianOfMedians fcmp ngreats (rank - nelem + ngreats) chunkSize greats            -- O(greats) where w.c. n = smalls+greats
+  where
+    -- require 3 passes over xs (last implicit over smalls, greats)
+    pivot = fromJust $ findColMedianWorker fcmp nelem chunkSize xs -- O(?)
+    (smalls, equals, greats) = mkArrayPartition $ partitionByPivot fcmp pivot xs -- O(n)
+    (nsmalls, ngreats) = (arraySize smalls, arraySize greats)
+    -- (nsmalls, ngreats) = (length smalls, length greats) -- O(n)
+    (pivotLowerRank, pivotUpperRank) = (nsmalls, nelem - ngreats - 1) -- O(1)
+
+-- | Choose approximate median by subsampling, no approximation guarantee exists.
+sicilianMedian :: Foldable t =>
+                  (a -> a -> Ordering) -- ^ compute Ordering of first w.r.t. latter
+               -> Int -- ^ size of local neighborhood where median is chosen
+               -> Int -- ^ number of elements in foldable
+               -> t a -> Maybe a
+sicilianMedian fcmp chunkSize nelem xs
+  | nelem <= 10*chunkSize = pickMiddle . L.sortBy fcmp $ toList xs
+  | otherwise = uncurry (flip (sicilianMedian fcmp chunkSize)) $ produceColMedians fcmp chunkSize xs
 
 groupsOfN :: Int -> [a] -> [[a]]
 groupsOfN n xs = g : if null xs' then [] else groupsOfN n xs'
@@ -60,21 +100,8 @@ pickMiddle xs = helper xs xs
     helper (y : _) [_,_] = Just y
     helper (_ : ys) (_ : _ : zs) = helper ys zs
 
-medianRank :: Int -> Int
-medianRank = floor . (% 2) . subtract 1
-
--- | selects the lower median from the list
-findColMedianWorker :: (a -> a -> Ordering) -> Size -> Size -> [a] -> Maybe a
-findColMedianWorker fcmp nelem chunkSize = medianOfMedians fcmp nelem' (medianRank nelem') chunkSize . map pickColMid . groupsOfN chunkSize
-  where
-    -- Really slow for what it does.  Ensure groups themselves are evaluated strictly
-    pickColMid = fromJust . pickMiddle . L.sortBy fcmp
-    nelem' = ceiling $ nelem % chunkSize
-    -- subtract one because it's #elements smaller, pick lower median
-    medRank = floor . (% 2) $ nelem' - 1
-
-findColMedianWorker' :: forall a t. (Foldable t) => (a -> a -> Ordering) -> Size -> Size -> t a -> Maybe a
-findColMedianWorker' fcmp nelem chunkSize = medianOfMedians fcmp nelem' (medianRank nelem') chunkSize . mkArray . produceColMedians fcmp chunkSize
+findColMedianWorker :: forall a t. (Foldable t) => (a -> a -> Ordering) -> Size -> Size -> t a -> Maybe a
+findColMedianWorker fcmp nelem chunkSize = medianOfMedians fcmp nelem' (medianRank nelem') chunkSize . mkArray . produceColMedians fcmp chunkSize
   where
     nelem' = ceiling $ nelem % chunkSize
     mkArray :: ([a], Int) -> A.Array Int a
@@ -98,42 +125,24 @@ produceColMedians fcmp nchunk = finishup . foldl' (\tup x -> processGroup $ addT
 arraySize :: (Num i, A.IArray a e, A.Ix i) => a i e -> i
 arraySize = (+ 1) . uncurry subtract . A.bounds
 
--- | select element at index rank as in the median-of-medians algorithm
--- adapted to also work with duplicate elements
-medianOfMedians :: Foldable t => (a -> a -> Ordering) -- ^ comparator: computes Ordering of the first with respect to the latter
-                -> Int -- ^ number of elements in list
-                -> Int -- ^ rank of the item to select
-                -> Int -- ^ number of elements in a column
-                -> t a -> Maybe a
-medianOfMedians fcmp nelem rank chunkSize xs
-  | null xs = Nothing
-  | nelem <= chunkSize = Just . (!! rank) . L.sortBy fcmp $ F.toList xs            -- O(1)
-  | pivotLowerRank <= rank && rank <= pivotUpperRank = Just pivot            -- O(1)
-  | rank < pivotLowerRank = medianOfMedians fcmp nsmalls rank chunkSize smalls            -- O(smalls)
-  | rank > pivotUpperRank = medianOfMedians fcmp ngreats (rank - nelem + ngreats) chunkSize greats            -- O(greats) where w.c. n = smalls+greats
+-- folds into reversed array indexed 0 to (n-1)
+fold2Array :: (Foldable t, A.IArray a e, A.Ix i, Num i) => t e -> a i e
+fold2Array = construct . foldl' countConcat (0, [])
   where
-    -- require 3 passes over xs (last implicit over smalls, greats)
-    pivot = fromJust $ findColMedianWorker' fcmp nelem chunkSize xs -- O(?)
-    (smalls, equals, greats) = partitionByPivot' fcmp pivot xs -- O(n)
-    (nsmalls, ngreats) = (arraySize smalls, arraySize greats)
-    -- (nsmalls, ngreats) = (length smalls, length greats) -- O(n)
-    (pivotLowerRank, pivotUpperRank) = (nsmalls, nelem - ngreats - 1) -- O(1)
+    countConcat (size, list) x = (size + 1, x : list)
+    construct (size, list) = A.listArray (0, fromIntegral (size - 1)) list
 
-sicilianMedian :: Foldable t => (a -> a -> Ordering) -> Int -> Int -> t a -> Maybe a
-sicilianMedian fcmp chunkSize nelem xs
-  | nelem <= 10*chunkSize = pickMiddle . L.sortBy fcmp $ toList xs
-  | otherwise = uncurry (flip (sicilianMedian fcmp chunkSize)) $ produceColMedians fcmp chunkSize xs
 
-pickMedian :: (a -> a -> Ordering) -> [a] -> Maybe a
-pickMedian fcmp xs = medianOfMedians fcmp (length xs) (medianRank $ length xs) 5 xs
+cons' :: a -> [a] -> [a]
+cons' x xs = x `seq` x : xs
 
-pickAtRank :: (a -> a -> Ordering) -> Rank -> [a] -> Maybe a
-pickAtRank fcmp rank xs = medianOfMedians fcmp (length xs) rank 5 xs
+type Compo a = ([a], Int)
 
--- partitions list into values smaller or equal and values greater than the lower median 
-partitionByMedian :: (a -> a -> Ordering) -> [a] -> Maybe (a,[a],[a])
-partitionByMedian fcmp xs = makePartition <$> pickMedian fcmp xs
-  where
-    makePartition med = let (smalls, equals, greats) = partitionByPivot' fcmp med xs
-                            (nsmalls, ngreats) = (arraySize smalls, arraySize greats)
-                        in if nsmalls <= ngreats then (med, A.elems equals ++ A.elems smalls, A.elems greats) else (med, A.elems smalls, A.elems equals ++ A.elems greats)
+emptyCompo :: Compo a
+emptyCompo = ([], 0)
+
+addToCompo :: Compo a -> a -> Compo a
+addToCompo (xs, n) x = (cons' x xs, n+1)
+
+compo2Array :: (Num i, A.Ix i, A.IArray a e) => Compo e -> a i e
+compo2Array (xs, n) = A.listArray (0, fromIntegral $ n - 1) xs
